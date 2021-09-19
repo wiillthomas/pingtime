1277f38f57d72bcf6c818d931c76347bb939b013
Will Thomas <wiillthomas@hotmail.com>
2021-09-19 08:39:57 +0100

    working now

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-import axios from "axios";
-import moment from "moment";
+// import axios from "axios";
+// import moment from "moment";
+const axios = require('axios')
+const moment = require('moment')
 
 const args = process.argv;
-
-let endpointTime = 0;
-for (let i = 0; i < 10; i += 1) {
-    const since = moment();
-    await axios.get(args[2]);
-    const end = moment();
-    console.log(endpointTime);
-    endpointTime += end.diff(since, "ms");
-}
-const avgResponse = endpointTime / 10;
-console.log(avgResponse)
+(async () => {
+    let endpointTime = 0;
+    for (let i = 0; i < 10; i += 1) {
+        const since = moment();
+        await axios.get(args[2]);
+        const end = moment();
+        console.log(endpointTime);
+        endpointTime += end.diff(since, "ms");
+    }
+    const avgResponse = endpointTime / 10;
+    console.log(avgResponse)
+})()
 
 // usage node app.js {{URL}]
